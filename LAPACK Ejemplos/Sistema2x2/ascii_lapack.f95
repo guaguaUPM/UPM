e8293a4b8a459d8c9a8ec4493e912ac24733160d
Fernando Ayats <ayatsfer@gmail.com>
2018-02-12 23:22:58 +0100

    Fer ejemplo lapack con prompt

---
+++ LAPACK Ejemplos/Sistema2x2/ascii_lapack.f95
@@ -0,0 +1,21 @@
+module ascii_art
+    ! http://http://patorjk.com/software/taag/#p=display&f=Georgi16
+contains
+    subroutine ascii_lapack
+        write(*,*) "____                                        ___       "
+        write(*,*) "`MM'                                        `MM       "
+        write(*,*) " MM                                          MM       "
+        write(*,*) " MM         ___  __ ____      ___     ____   MM   __  "
+        write(*,*) " MM       6MMMMb `M6MMMMb   6MMMMb   6MMMMb. MM   d'  "
+        write(*,*) " MM      8M'  `Mb MM'  `Mb 8M'  `Mb 6M'   Mb MM  d'   "
+        write(*,*) " MM          ,oMM MM    MM     ,oMM MM    `' MM d'    "
+        write(*,*) " MM      ,6MM9'MM MM    MM ,6MM9'MM MM       MMdM.    "
+        write(*,*) " MM      MM'   MM MM    MM MM'   MM MM       MMPYM.   "
+        write(*,*) " MM    / MM.  ,MM MM.  ,M9 MM.  ,MM YM.   d9 MM  YM.  "
+        write(*,*) "_MMMMMMM `YMMM9'YbMMYMMM9  `YMMM9'Yb.YMMMM9 _MM_  YM._"
+        write(*,*) "                  MM                                  "
+        write(*,*) "                  MM                                  "
+        write(*,*) "                 _MM_                                 "
+        write(*,*)
+    end subroutine ascii_lapack
+end module ascii_art
--- LAPACK Ejemplos/Sistema2x2/main.f95
+++ LAPACK Ejemplos/Sistema2x2/main.f95
@@ -1,28 +1,41 @@
 program pruebas
+use ascii_art
 implicit none
 
-integer :: INFO
-real*8 :: A(2,2), B(2,1), IPIV(2,2)
+integer :: INFO, i, j, IPIV(2,2)
+real*8 :: A(2,2), B(2)
 
-A(1,1) = 1.d0
-A(1,2) = 1.d0
-A(2,1) = 0.d0
-A(2,2) = 2.d0
+call ascii_lapack
 
-B(1,1) = 2.d0
-B(2,1) = 3.d0 
+IPIV = 0
 
-write(*,*) A(1,:), "|", B(1,:)
-write(*,*) A(2,:), "|", B(2,:)
+write(*,*) "Ejemplo de resolución de sistema de dos variables con dos incógnitas"
+write(*,*) "Matricialmente AX=B"
+write(*,*) "Introduzca los valores de las filas y columnas de A y B"
+
+! EN EL FUTURO SE EXPANDIRÁ A TAMAXOS NxN
+do i = 1, 2
+    do j = 1, 2
+        write(*,*) "A, fila", i, "columna", j
+        read(*,*) A(i,j)
+    end do
+end do
+
+do i = 1, 2
+    write(*,*) "B, componente", i
+    read(*,*) B(i)
+end do
+
+write(*,*)
+write(*,*) "Tu sistema es:"
+write(*,*) A(1,:), "|", B(1)
+write(*,*) A(2,:), "|", B(2)
+
 
 call dgesv(2, 1, A, 2, IPIV, B, 2, INFO)
-
-write(*,*) A(1,:), "|", B(1,:)
-write(*,*) A(2,:), "|", B(2,:)
-
-write(*,*) IPIV(1,:)
-write(*,*) IPIV(2,:)
-
-write(*,*) info
+if(info==0) then
+    write(*,*)
+    write(*,*) B
+end if
 
 end program pruebas
