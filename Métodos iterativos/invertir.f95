075f6eac7df90aaeecb3ef41b1b90e8297dbb6f7
Fernando Ayats <ayatsfer@gmail.com>
2018-02-13 19:32:01 +0100

    inversa

--- Métodos iterativos/invertir.f95
+++ Métodos iterativos/invertir.f95
@@ -1,6 +1,15 @@
 module invertir
 contains
     function inversa(ENTRADA)
+        real*8, intent(in) :: ENTRADA(:,:)
+        real*8, allocatable :: inversa(:,:)
+        integer :: tamano, IPIV(:,:), i
+        tamano = size(ENTRADA(1,:))
+        allocate(inversa(tamano, tamano))
+        allocate(IPIV(tamano, tamano))
+
+        inversa = ENTRADA
+        call DGETRI(tamano, inversa, tamano, IPIV, tamano, -1, i)
     
     end function inversa
 end module invertir
