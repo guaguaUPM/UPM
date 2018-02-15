815815ddd94b17eb2241a1e80214f56607d4a05c
LucasJ <lucasj0226@gmail.com>
2018-02-15 13:19:55 +0100

    fer

--- Modulos/main.f95
+++ Modulos/main.f95
@@ -10,6 +10,17 @@ allocate (MA(m,m))
 allocate (Vb(m))
 allocate (Vx(m))
 
+MA(1,1)=1
+MA(1,2)=1
+MA(1,3)=1
+MA(2,1)=1
+MA(2,2)=1
+MA(2,3)=1
+MA(3,1)=1
+MA(3,2)=1
+MA(3,3)=1
+
+
 call Gauss(MA,Vb,Vx)
 
     write(*,*) 'Solución'
