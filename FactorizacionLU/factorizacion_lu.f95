e36473d02ab86f001965d6b0b93d610ae97719da
Fernando Ayats <ayatsfer@gmail.com>
2018-02-17 21:29:12 +0100

    rebase

--- FactorizacionLU/factorizacion_lu.f95
+++ FactorizacionLU/factorizacion_lu.f95
@@ -6,25 +6,42 @@ subroutine factorizar(ENTRADA, L, U, TAMANO)
 
     ! La matriz entrada no debe tener ceros en la diagonal
 
-    real*8, intent(in)   ::  ENTRADA(TAMANO, TAMANO)
-    real*8, intent(out)  ::  L(TAMANO, TAMANO), U(TAMANO, TAMANO)
+    real*8, intent(in)   ::  ENTRADA(:,:)
+    real*8, intent(out), allocatable   ::  L(:,:), U(:,:)
+    real*8, allocatable  :: m(:,:)
     integer, intent(in)  ::  TAMANO
     integer              :: i, j
 
-    write(*,*) "1"
+    write(*,*) TAMANO
+    allocate(L(tamano,tamano))
+    allocate(U(tamano,tamano))
+    allocate(m(tamano,tamano))
+    write(*,*) "1" !! DEBUG
 
     U=-1.d0
     L=-1.d0
 
+    ! Se asignan los valores por defecto a las matrices (1 en la diagonal de U)
     do i = 1, TAMANO
         U(i,i) = 1.d0
+
         do j = i+1, TAMANO
-            U(i,j) = 0.d0
-        end do
-        do j = 0, i-1
             L(i,j) = 0.d0
         end do
+        do j = 1, i-1
+            U(i,j) = 0.d0
+        end do
     end do
+    L(1,1) = ENTRADA(1,1)
+
+    do j=1, tamano-1
+        do i=j+1, tamano
+
+            M(i,j) = U(i,j)/J(j,j)
+            U(i,:) = U(i,:) - m(i,j) * U(j,:)
+        end do
+    end do
+
 
 end subroutine factorizar
 end module factorizacion_lu
--- FactorizacionLU/main.f95
+++ FactorizacionLU/main.f95
@@ -3,17 +3,19 @@ use ascii_art
 use factorizacion_lu
 implicit none
 
-
-
 integer::n
 real(8),allocatable:: A(:,:),L(:,:),U(:,:)
 integer::i,j 
 
 call ascii
+
 write(*,*) "Introduce el tamaño de la matriz:"
 read(*,*) n
 
-allocate (A(n,n))
+allocate(A(n,n))
+allocate(L(n,n))
+allocate(U(n,n))
+
 do i = 1, N
     do j = 1, N
         write(*,fmt='(a7,1x,i0,1x,a7,1x,i0)') "A, fila", i, "columna", j
@@ -21,13 +23,15 @@ do i = 1, N
     end do
 end do
 
+
 call factorizar(A, L, U, N)
-                                      !Debug que muestra L y U
+
+!Debug que muestra L y U
 do i=1, N
     write(*,*) L(i,:), "|", U(i, :)
 end do
 
-                                      !Solucion
+!Solucion
 
 
 end program mates
