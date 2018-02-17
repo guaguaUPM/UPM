3d42f490f712333c534c58ae2152a220476239f4
Fernando Ayats <ayatsfer@gmail.com>
2018-02-17 22:27:10 +0100

    gauss solver

--- FactorizacionLU/factorizacion_lu.f95
+++ FactorizacionLU/factorizacion_lu.f95
@@ -1,47 +1,81 @@
 module factorizacion_lu
 contains
 
-subroutine factorizar(ENTRADA, L, U, TAMANO)
+subroutine factorizar(ENTRADA, LU, TAMANO)
     implicit none
 
     ! La matriz entrada no debe tener ceros en la diagonal
 
     real*8, intent(in)   ::  ENTRADA(:,:)
-    real*8, intent(out), allocatable   ::  L(:,:), U(:,:)
-    real*8, allocatable  :: m(:,:)
+    real*8, allocatable, intent(out)  ::  LU(:,:)
     integer, intent(in)  ::  TAMANO
-    integer              :: i, j
+    integer              ::  i, j, k, s
+    real*8               ::  suma
 
-    write(*,*) TAMANO
-    allocate(L(tamano,tamano))
-    allocate(U(tamano,tamano))
-    allocate(m(tamano,tamano))
-    write(*,*) "1" !! DEBUG
+    allocate(LU(tamano, tamano))
+    LU = ENTRADA
 
-    U=-1.d0
-    L=-1.d0
-
-    ! Se asignan los valores por defecto a las matrices (1 en la diagonal de U)
-    do i = 1, TAMANO
-        U(i,i) = 1.d0
-
-        do j = i+1, TAMANO
-            L(i,j) = 0.d0
+    do j = 1, tamano
+        do i = 1, j
+          if (i == 1) then
+            LU(i,j) = LU(i, j)
+          else
+             suma = 0.0d0
+            do k = 1, i-1
+               suma = suma + LU(i,k)*LU(k,j)
+            end do
+                LU(i,j) = LU(i,j) - suma
+          end if
         end do
-        do j = 1, i-1
-            U(i,j) = 0.d0
-        end do
-    end do
-    L(1,1) = ENTRADA(1,1)
-
-    do j=1, tamano-1
-        do i=j+1, tamano
-
-            M(i,j) = U(i,j)/J(j,j)
-            U(i,:) = U(i,:) - m(i,j) * U(j,:)
-        end do
-    end do
-
+        if ( j < tamano) then
+          do s = 1, tamano-j
+            i = j + s
+            if (j == 1) then
+                LU(i,j) = LU(i,j)/LU(j,j)
+            else
+              suma = 0.0d0
+              do k = 1, j-1
+                suma = suma + LU(i,k)*LU(k,j)
+              end do
+                LU(i,j) = (LU(i,j) - suma)/LU(j,j)
+            end if
+          end do
+        end if
+       end do
 
 end subroutine factorizar
+
+subroutine resolver(B, LU, X, TAMANO)
+    implicit none
+    real*8, intent(in) :: LU(:,:), B(:)
+    real*8, intent(out), allocatable :: X(:)
+    real*8, allocatable :: y(:)
+    real*8 :: suma
+    integer, intent(in) :: tamano
+    integer :: i, j, s
+
+    allocate(x(tamano))
+    allocate(y(tamano))
+
+
+    y(1) = b(1)
+    do i = 2, tamano
+        suma = 0.0
+        do j = 1, i-1
+            suma = suma + LU(i,j)*y(j)
+        end do
+        y(i) = b(i) - suma
+    end do
+    i = tamano
+    j = tamano
+    x(i) = y(i)/LU(i,j)
+    do s = 1, tamano -1
+        i = tamano - s
+        suma = 0.0
+        do j = i+1, tamano
+            suma = suma + LU(i,j)*x(j)
+        end do
+        x(i) = (y(i) - suma)/LU(i,i)
+    end do
+end subroutine resolver
 end module factorizacion_lu
--- FactorizacionLU/main.f95
+++ FactorizacionLU/main.f95
@@ -4,7 +4,7 @@ use factorizacion_lu
 implicit none
 
 integer::n
-real(8),allocatable:: A(:,:),L(:,:),U(:,:)
+real(8),allocatable:: A(:,:),L(:,:),U(:,:), LU(:,:), X(:), B(:)
 integer::i,j 
 
 call ascii
@@ -13,8 +13,9 @@ write(*,*) "Introduce el tamaño de la matriz:"
 read(*,*) n
 
 allocate(A(n,n))
-allocate(L(n,n))
-allocate(U(n,n))
+allocate(LU(n,n))
+allocate(X(n))
+allocate(B(n))
 
 do i = 1, N
     do j = 1, N
@@ -22,16 +23,31 @@ do i = 1, N
         read(*,*) A(i,j)
     end do
 end do
-
-
-call factorizar(A, L, U, N)
-
-!Debug que muestra L y U
-do i=1, N
-    write(*,*) L(i,:), "|", U(i, :)
+do i = 1, N
+    write(*,fmt='(a13,1x,i0)') "B, componente", i
+    read(*,*) B(i)
 end do
 
-!Solucion
+write(*,*) "Su sistema es:"
+do i=1, N
+    write(*,*) A(i,:), "|", B(i)
+end do
+
+
+call factorizar(A, LU, N)
+
+!Debug que muestra L y U
+write(*,*) "Descomposición LU:"
+write(*,*)
+do i=1, N
+    write(*,*) LU(i,:)
+end do
+
+write(*,*)
+write(*,*) "Resolución:"
+call resolver(B, LU, X, N)
+write(*,*)
+write(*,*) X
 
 
 end program mates
