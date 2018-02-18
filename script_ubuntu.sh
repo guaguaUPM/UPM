b3eddcc93a3f5f553a07e222b1820f0756cf6172
Fernando Ayats <ayatsfer@gmail.com>
2018-02-18 18:49:42 +0100

    eliminado script

--- script_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt -y update
-sudo apt -u upgrade
-sudo apt -y install build-essential gfortran
-
-wget http://www.netlib.org/lapack/lapack-3.8.0.tar.gz
-tar -xvzf lapack-3.8.0.tar.gz
-cd lapack*
