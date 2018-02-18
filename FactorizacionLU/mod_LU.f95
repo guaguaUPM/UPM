0ef62cf5095b0d9f15ff588da8a0812a2c2f4fea
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-02-18 18:46:38 +0100

    actualiza

--- FactorizacionLU/mod_LU.f95
+++ FactorizacionLU/mod_LU.f95
@@ -47,7 +47,72 @@ subroutine factorizacion (A,L,U,n)
         L(k+1,k+1) = A(k+1,k+1)-sum   
     end do                                           
 end subroutine
-
+    subroutine pivotar(ENTRADA, SALIDA, B_ENTRADA, B_SALIDA, PERMUTACION, TAMANO)
+        implicit none
+    
+        real*8, intent(in)                :: entrada(:,:), b_entrada(:)
+        real*8, allocatable, intent(out)  :: salida(:,:) , b_salida(:)
+        integer, allocatable, intent(out) :: permutacion(:,:)
+        integer, intent(in)               :: tamano
+    
+        integer                           :: i, fila_max, j
+        real*8                            :: valor_max, b_auxiliar
+        real*8, allocatable               :: AUXILIAR(:)
+        integer, allocatable              :: permutacion_auxiliar(:)
+    
+        allocate(salida(tamano, tamano))
+        allocate(b_salida(tamano))
+        allocate(permutacion(tamano, tamano))
+        allocate(AUXILIAR(tamano))
+        allocate(permutacion_auxiliar(tamano))
+        
+        SALIDA=ENTRADA
+        B_SALIDA = B_ENTRADA
+    
+        permutacion = 0
+        do i=1, tamano
+            permutacion(i,i) = 1
+        end do
+    
+        ! Bucle que recorre la diagonal
+        do i=1, tamano
+            ! Si se encuetra un 0 en la diagonal
+            if(salida(i,i) == 0.d0) then
+                ! Resetea el valor maximo, se usarán siempre numeros positivos
+                valor_max = 0.d0
+                ! Se recorren las posiciones por debajo de la diagonal para encontrar el maximo valor
+                do j=i+1, tamano
+                    if( ABS(SALIDA(j,i)) >= valor_max ) then
+                        fila_max = j
+                        valor_max = salida(j,i)
+                    end if
+                end do
+    
+                ! write(*,*) "FILA MAX:", fila_max, valor_max  ! DEBUG
+    
+                ! write(*,*) "CAMBIO", i, "por", fila_max      ! DEBUG
+    
+                ! Cambio por rotacion triangular de la matriz
+                AUXILIAR(:)        = salida(i,:)
+                salida(i,:)        = salida(fila_max, :)
+                salida(fila_max,:) = AUXILIAR(:)
+    
+                ! Cambio por rotacion triangular de la matriz permutacion
+                permutacion_auxiliar(:)   = PERMUTACION(i,:)
+                PERMUTACION(i,:)          = PERMUTACION(fila_max,:)
+                PERMUTACION(fila_max,:)   = permutacion_auxiliar(:)
+    
+                ! Cambio por rotacion triangular del vector B
+                b_auxiliar         = b_salida(i)
+                b_salida(i)        = b_salida(fila_max)
+                b_salida(fila_max) = b_auxiliar
+    
+                ! write(*,*) "AUXILIAR vale", AUXILIAR   ! DEBUG
+            end if
+        end do
+    
+    end subroutine pivotar
+        
     subroutine invertir(U,m,Ui)
         integer,intent(in)::m 
         integer::i,j
