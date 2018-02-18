103871530bcc35fec797dede8083f6816ccb152f
Fernando Ayats <ayatsfer@gmail.com>
2018-02-18 19:00:34 +0100

    script

--- Guía_LAPACK/script.sh
+++ Guía_LAPACK/script.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 sudo apt -y update
 sudo apt -y upgrade
+sudo apt -y install make build-essentail gfortran
+
+wget http://www.netlib.org/lapack/lapack-3.8.0.tar.gz
+tar -xvzf lapack-3.8.0.tar.gz
+cd lapack-3.8.0
