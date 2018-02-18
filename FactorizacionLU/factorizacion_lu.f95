51a6f708dbc958bd1cc6c3bcf17f05809f90e066
Fernando Ayats <ayatsfer@gmail.com>
2018-02-18 13:09:36 +0100

    lu fernando

--- FactorizacionLU/factorizacion_lu.f95
+++ FactorizacionLU/factorizacion_lu.f95
@@ -10,8 +10,8 @@ subroutine factorizar(ENTRADA, LU, TAMANO)
     real*8, allocatable, intent(out)  ::  LU(:,:)
     integer, intent(in)               ::  TAMANO
 
-    integer              ::  i, j, k, s
-    real*8               ::  suma
+    integer                           ::  i, j, k, s
+    real*8                            ::  suma
 
     allocate(LU(tamano, tamano))
     LU = ENTRADA
@@ -49,13 +49,13 @@ end subroutine factorizar
 subroutine resolver(B, LU, X, TAMANO)
     implicit none
 
-    real*8, intent(in) ::  B(:), LU(:,:)
-    real*8, intent(out), allocatable :: X(:)
-    integer, intent(in) :: TAMANO
+    real*8, intent(in)                :: B(:), LU(:,:)
+    real*8, intent(out), allocatable  :: X(:)
+    integer, intent(in)               :: TAMANO
 
-    real*8, allocatable :: y(:)
-    real*8 :: suma
-    integer :: i, j, s
+    real*8, allocatable               :: y(:)
+    real*8                            :: suma
+    integer                           :: i, j, s
 
     allocate(x(tamano))
     allocate(y(tamano))
@@ -80,5 +80,70 @@ subroutine resolver(B, LU, X, TAMANO)
         x(i) = (y(i) - suma)/LU(i,i)
     end do
 end subroutine resolver
+
+subroutine pivotar(ENTRADA, SALIDA, B_ENTRADA, B_SALIDA, PERMUTACION, TAMANO)
+    implicit none
+
+    real*8, intent(in)                :: entrada(:,:), b_entrada(:)
+    real*8, allocatable, intent(out)  :: salida(:,:) , b_salida(:)
+    integer, allocatable, intent(out) :: permutacion(:,:)
+    integer, intent(in)               :: tamano
+
+    integer                           :: i, fila_max, j
+    real*8                            :: valor_max, b_auxiliar
+    real*8, allocatable               :: AUXILIAR(:)
+    integer, allocatable              :: permutacion_auxiliar(:)
+
+    allocate(salida(tamano, tamano))
+    allocate(b_salida(tamano))
+    allocate(permutacion(tamano, tamano))
+    allocate(AUXILIAR(tamano))
+    allocate(permutacion_auxiliar(tamano))
     
+    SALIDA=ENTRADA
+    B_SALIDA = B_ENTRADA
+
+    permutacion = 0
+    do i=1, tamano
+        permutacion(i,i) = 1
+    end do
+
+    ! Bucle que recorre la diagonal
+    do i=1, tamano
+        ! Si se encuetra un 0 en la diagonal
+        if(salida(i,i) == 0.d0) then
+            ! Resetea el valor maximo, se usarán siempre numeros positivos
+            valor_max = 0.d0
+            ! Se recorren las posiciones por debajo de la diagonal para encontrar el maximo valor
+            do j=i+1, tamano
+                if( ABS(SALIDA(j,i)) >= valor_max ) then
+                    fila_max = j
+                    valor_max = salida(j,i)
+                end if
+            end do
+
+            ! write(*,*) "FILA MAX:", fila_max, valor_max  ! DEBUG
+
+            ! write(*,*) "CAMBIO", i, "por", fila_max      ! DEBUG
+
+            ! Cambio por rotacion triangular de la matriz
+            AUXILIAR(:)        = salida(i,:)
+            salida(i,:)        = salida(fila_max, :)
+            salida(fila_max,:) = AUXILIAR(:)
+
+            ! Cambio por rotacion triangular de la matriz permutacion
+            permutacion_auxiliar(:)   = PERMUTACION(i,:)
+            PERMUTACION(i,:)          = PERMUTACION(fila_max,:)
+            PERMUTACION(fila_max,:)   = permutacion_auxiliar(:)
+
+            ! Cambio por rotacion triangular del vector B
+            b_auxiliar         = b_salida(i)
+            b_salida(i)        = b_salida(fila_max)
+            b_salida(fila_max) = b_auxiliar
+
+            ! write(*,*) "AUXILIAR vale", AUXILIAR   ! DEBUG
+        end if
+    end do
+
+end subroutine pivotar
 end module factorizacion_lu
--- FactorizacionLU/main.f95
+++ FactorizacionLU/main.f95
@@ -1,22 +1,33 @@
 program mates
 use ascii_art
 use factorizacion_lu
+use algebra
 implicit none
 
 integer::n
-real(8),allocatable:: A(:,:), LU(:,:), B(:), X(:)
-real :: cpu_start,cpu_finish
-integer::i,j 
+real(8),allocatable    :: A(:,:), A_pivotada(:,:), LU(:,:), B(:), B_pivotada(:), X(:), X_pivotada(:), L(:,:), U(:,:)
+integer, allocatable   :: PERMUTACION(:,:)
+real                   :: cpu_start,cpu_finish
+integer                ::i,j 
 
 call ascii
 
 write(*,*) "Introduce el tamaño de la matriz:"
 read(*,*) n
 
+! mold es para no tener que poner (n,n) en cada una, queda mas elegante
 allocate(A(n,n))
-allocate(LU(n,n))
-allocate(X(n))
+allocate(LU, mold=A)
+allocate(A_pivotada, mold=A)
+allocate(L, mold=A)
+allocate(U, mold=A)
+allocate(PERMUTACION(n,n))
+
 allocate(B(n))
+allocate(B_pivotada, mold=B)
+allocate(X, mold=B)
+allocate(X_pivotada, mold=B)
+
 
 ! --------- SE PIDEN DATOS AL USUARIO
 do i = 1, N
@@ -36,23 +47,51 @@ do i=1, N
 end do
 ! -----------------------------
 
+
+
+
 call cpu_time(cpu_start) ! Se empiza a contar el tiempo
 
-call factorizar(A, LU, N)
-
-! Debug que muestra L y U
+! Subrutina de pivotación, devuelve la matriz pivotada con la matriz pivotacion
+! FALTA POR HACER: si el 0 está en la útima posición
+call pivotar(A, A_pivotada, B, B_pivotada, PERMUTACION, N)
 write(*,*)
-write(*,*) "Descomposición LU:"
+write(*,*) "Matriz de pivotación y sistema pivotado:"
+do i=1, N
+    write(*,*) PERMUTACION(i,:), "||", A_pivotada(i,:), "|", B_pivotada(i)
+end do
+
+! Descomposicion por el método Fernando
+! A BORRAR, el metodo Lucas funciona mejor
+call factorizar(A_pivotada, LU, N)
+write(*,*)
+write(*,*) "Descomposición LU, método Fernando"
 do i=1, N
     write(*,*) LU(i,:)
 end do
 
+! Descomposicion por el método Lucas
+call factorizacion(A_pivotada, L, U, N)
+write(*,*)
+write(*,*) "Descomposición LU, método LUCAS:"
+do i=1, N
+    write(*,*) L(i,:), "|", U(i,:)
+end do
+
+
+! Resolucion por metodo Fernando
+! A BORRAR Y SUSTITUIR POR METODO FERNANDO
 write(*,*)
 write(*,*) "Resolución:"
-call resolver(B, LU, X, N)
+call resolver(B_pivotada, LU, X_pivotada, N)
+
+! Se despivota el vector resolución
+X = matmul(X_pivotada, PERMUTACION)
 
 call cpu_time(cpu_finish)
 
+write(*,*) "X pivot vale", X_pivotada
+write(*,*)
 write(*,*) X, "Se tardó en segundos:", cpu_finish-cpu_start
 
 
