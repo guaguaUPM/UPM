9cfa7e85e2530ef346ee5ffbf4923d1219b241d5
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-02-20 11:47:15 +0100

    alrx_practica

---
+++ Practica01/main.f95
@@ -0,0 +1,17 @@
+program main
+use algebra_lineal
+implicit none
+
+!PREGUNTA 1
+!Sean la matriz A de tamaño 100 x 100 y el vector b de tamaño 100 definidos como:
+
+
+
+
+
+
+
+
+
+
+end program main
