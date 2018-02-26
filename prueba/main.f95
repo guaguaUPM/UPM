bb28da0b85b2f2b9819321e3ada7da7ed6529d51
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-02-26 09:22:58 +0100

    prueba

---
+++ prueba/main.f95
@@ -0,0 +1,24 @@
+program main
+use algebra_lineal
+implicit none
+
+real*8, allocatable :: A(:,:), At(:,:), L(:,:), U(:,:), b(:), X(:)
+integer :: i, j
+real*8 :: tol
+integer, parameter :: N=4 !DEPENDE DEL EXAMEN
+allocate(A(N,N))
+allocate(At(N,N))
+allocate(L(N,N))
+allocate(U(N,N))
+allocate(b(N))
+allocate(X(N))
+
+do i=1,n
+    do j=1,n
+        write(*,*)"Escriba la posicion",i,j
+        read(*,*) A(i,j)
+    enddo
+enddo
+
+    write(*,*) inversa(A)
+end program main
