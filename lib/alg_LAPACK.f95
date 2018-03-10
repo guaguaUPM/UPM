596e611cb9f373eba1ec1cb9fd038abba1c5ae60
Fernando Ayats <ayatsfer@gmail.com>
2018-03-10 20:22:02 +0100

    resolucion de sistemas por metodos

---
+++ lib/alg_LAPACK.f95
@@ -0,0 +1,14 @@
+subroutine resolver_LAPACK(A,B,X,N)
+    implicit none
+    integer, intent(in)  :: N
+    real*8, intent(in)   :: A(N,N), B(N)
+    real*8, intent(out)  :: X(N)
+    integer              :: IPIV(N,N), info
+    X=B
+    call DGESV(N,1,A,N,IPIV,X,N,info)
+    if(info < 0) then
+        write(*,*) "El argumento", -info, "es incorrecto"
+    else if (info > 0) then
+        write(*,*) "La factorización ha fallado"
+    end if
+end subroutine resolver_LAPACK
