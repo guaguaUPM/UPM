ea1051f0b58f943d668b355304e220e65fb32f1c
Fernando Ayats <ayatsfer@gmail.com>
2018-03-13 09:05:36 +0100

    base autovalores

---
+++ lib/alg_autovalores.f95
@@ -0,0 +1,6 @@
+subroutine auto_potencia (A,AUTOVALOR,N)
+    implicit none
+    integer, intent(in) :: N
+    real*8, intent(in) :: A(N,N)
+    real*8, intent(out) :: AUTOVALOR
+end subroutine auto_potencia
