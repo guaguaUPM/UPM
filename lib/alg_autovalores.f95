0a52e130a21399187dfa6127dfe49499f2159612
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-03-13 12:33:45 +0100

    alex_autovalores

--- lib/alg_autovalores.f95
+++ lib/alg_autovalores.f95
@@ -1,22 +1,35 @@
-subroutine auto_potencia (A,AUTOVALOR,TOL,N)
+subroutine auto_potencia (A,AUTOVECTOR,TOL,N)
     implicit none
     ! Variables de entrada/salida
     integer, intent(in) :: N
     real*8, intent(in)  :: A(N,N), TOL
-    real*8, intent(out) :: AUTOVALOR
+    real*8, intent(out) :: AUTOVECTOR(N)
+
     ! Variables propias
-    real*8 :: Q(N), Q_ANTERIOR(N), error, norma, norma_anterior
+    real*8 :: Q(N), Q_ANTERIOR(N), norma, resto(N)
+    integer :: i, j, maxiter
 
-    Q=(1.d0)
-    call norma2(norma,Q,N)
-    Q = Q/norma
-    write(*,*) Q
+    do i = 1, maxiter
+        Q=(1.d0)
+        call norma2(norma,Q,N)
+        Q = Q/norma
+        write(*,*) Q
 
-    Q_ANTERIOR = Q
+        Q_ANTERIOR = Q
 
-    Q = matmul(A,Q)
-    call norma2(norma,Q,N)
-    Q = Q/norma
-    write(*,*) Q
+        Q = matmul(A,Q)
+        call norma2(norma,Q,N)
+        Q = Q/norma
+        write(*,*) Q
+        
+        resto = Q - Q_ANTERIOR
+
+        do j=1,N
+            if( resto(j) <tol) then
+                AUTOVECTOR = Q
+                return
+            endif
+        enddo
+    enddo
 
 end subroutine auto_potencia
