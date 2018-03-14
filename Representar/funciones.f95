f2e3f5ae14a108eaffa5f94829829bc03de52cf1
Fernando Ayats <ayatsfer@gmail.com>
2018-03-14 19:40:01 +0100

    Representar funciones

---
+++ Representar/funciones.f95
@@ -0,0 +1,9 @@
+module funciones
+contains
+    function parabola(X)
+        real*8 :: X
+        real*8 :: parabola
+
+        parabola = X**2
+    end function parabola
+end module funciones
---
+++ Representar/main.f95
@@ -0,0 +1,9 @@
+program main
+use funciones
+implicit none
+
+write(*,*) parabola(3.d0)
+
+call represtarR1_R1(parabola,-5.d0,5.d0,10)
+
+end program main
