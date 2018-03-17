f28d09889c586c197e0796c8187971e8fe7d17e6
Fernando Ayats <ayatsfer@gmail.com>
2018-03-17 20:08:48 +0100

    cambiado a arcotangente

--- Derivacion/main.f95
+++ Derivacion/main.f95
@@ -6,9 +6,9 @@ real*8 :: m, x0
 
 write(*,*) "En que punto desea la derivada?"
 read(*,*) x0
-call derivada1_cent1(parabola,x0,m)
+call derivada1_cent1(arcotangente,x0,m)
 write(*,*) "La derivada primera centrada vale:", m
 
-call represtarR1_R1_tangente(parabola, -5.d0,5.d0, 30, x0,m)
+call represtarR1_R1_tangente(arcotangente, -5.d0,5.d0, 30, x0,m)
 
 end program main
