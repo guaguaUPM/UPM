2bd0671329b9e13b5928dd6d45c1337816a9703f
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-03-18 22:35:58 +0100

    alex

--- Calculo de autovalores/main.f95
+++ Calculo de autovalores/main.f95
@@ -1,18 +1,24 @@
 program autovalores
     implicit none
-    real*8, allocatable :: A(:,:)
-    real*8 :: autovalor, tol
+    real*8, allocatable :: A(:,:), q0(:)
+    real*8 :: autovalor_max, autovalor_min, tol
     integer :: N
 
-    
-    ! Se piden los datos de la matriz al usuario
+    ! Se piden los datos de la matriz y q0 al usuario
     write(*,*) "¿Que tamano de matriz desea?"
     read(*,*) N
     allocate(A(N,N))
+    allocate(q0(N))
+    q0 = 1.d0
     call pedir_matrix(A,N)
     call write_A(A,N)
     ! -----------------------------------------
 
-    call auto_potencia(A,autovalor,tol,N)
+    tol = 0.000005d0
+    call auto_potencia(A, autovalor_max, tol, q0, N)
+    call auto_potencia_inversa(A, autovalor_min, tol, q0, N)
+
+    write(*,*) "Autovalor Maximo:", autovalor_max
+    write(*,*) "Autovalor Minimo:", autovalor_min
 
 end program autovalores
--- lib/alg_autovalores.f95
+++ lib/alg_autovalores.f95
@@ -11,10 +11,10 @@ subroutine auto_potencia (A, AUTOVALOR, TOL, Q0, N)
     real*8, intent(out) :: AUTOVALOR
 
     ! Variables propias
-    real*8 :: Q(N), Q_ANTERIOR(N), norma, resto(N), AUTOVECTOR(N)
+    real*8 :: Q(N), Q_ANTERIOR(N), norma, norma_num, norma_den, resto(N), AUTOVECTOR(N)
     integer :: i, maxiter
 
-    maxiter = 10
+    maxiter = 999999
 
     Q = Q0
     call norma2(norma,Q,N)
@@ -22,6 +22,7 @@ subroutine auto_potencia (A, AUTOVALOR, TOL, Q0, N)
         Q = Q/norma
     endif
     write(*,*) Q
+    read(*,*)
 
     !   CALCULO DEL AUTOVECTOR
     do i = 1, maxiter
@@ -33,15 +34,20 @@ subroutine auto_potencia (A, AUTOVALOR, TOL, Q0, N)
         call norma2(norma,Q,N)
         Q = Q/norma
         write(*,*) Q
+        read(*,*)
         
         resto = Q - Q_ANTERIOR
+        call norma2(norma_num, resto, N)
+        call norma2(norma_den, Q, N)
 
-        if( maxval(resto) <tol) then
+        if( (norma_num/norma_den) <= tol) then
             AUTOVECTOR = Q
             exit
         endif
     enddo
     
+    AUTOVECTOR = Q
+
     !   CALCULO DEL AUTOVALOR ASOCIADO (coef. de Rayleigh)
     AUTOVALOR = DOT_PRODUCT(AUTOVECTOR, matmul(A, AUTOVECTOR)) / DOT_PRODUCT(AUTOVECTOR, AUTOVECTOR)
 
@@ -68,26 +74,31 @@ subroutine auto_potencia_inversa (A, AUTOVALOR, TOL, Q0, N)
         Q = Q/norma
     endif
     write(*,*) Q
+    read(*,*)
 
     !   CALCULO DEL AUTOVECTOR
     do i = 1, maxiter
 
         Q_ANTERIOR = Q
 
+        !call gauss_triangular (A, Q_ANTERIOR, Q, N)
         call resolver_gauss (A, Q_ANTERIOR, Q, N)
 
         call norma2(norma,Q,N)
         Q = Q/norma
         write(*,*) Q
+        read(*,*)
         
         resto = Q - Q_ANTERIOR
 
-        if( maxval(resto) <tol) then
-            AUTOVECTOR = Q
-            exit
-        endif
+        !if( maxval(resto) <tol) then
+            !AUTOVECTOR = Q
+            !exit
+        !endif
     enddo
     
+    AUTOVECTOR = Q
+
     !   CALCULO DEL AUTOVALOR ASOCIADO (coef. de Rayleigh)
     AUTOVALOR = DOT_PRODUCT(AUTOVECTOR, matmul(A, AUTOVECTOR)) / DOT_PRODUCT(AUTOVECTOR, AUTOVECTOR)
 
