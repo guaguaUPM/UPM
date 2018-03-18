8052b00de72e051248c20a515c4f33dac5607a8f
Fernando Ayats <ayatsfer@gmail.com>
2018-03-18 21:41:59 +0100

    corregido calculo derivada

--- Derivacion/main.f95
+++ Derivacion/main.f95
@@ -6,9 +6,9 @@ real*8 :: m, x0
 
 write(*,*) "En que punto desea la derivada?"
 read(*,*) x0
-call derivada1_prog2(seno,x0,m,1.0d-15)
+call derivada1_prog1(parabola,x0,m,SQRT(EPSILON(X0)))
 write(*,*) "La derivada primera centrada vale:", m
 
-call represtarR1_R1_tangente(seno, -3.d0,3.d0, 80, x0,m)
+call represtarR1_R1_tangente(parabola, -3.d0,3.d0, 80, x0,m)
 
 end program main
