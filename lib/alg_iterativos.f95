137ad89352a2a3f53aaec533fc2c9dd54591bcf8
alvaro234 <alvaroglezvilla@gmail.com>
2018-03-18 19:24:13 +0100

    d

--- lib/alg_iterativos.f95
+++ lib/alg_iterativos.f95
@@ -133,3 +133,47 @@ subroutine resolver_gauss_seidel (A, Xfinal, b, tol, N)
         !endif     
     enddo      
 end subroutine resolver_gauss_seidel
+
+    subroutine resolver_gauss_seidel (A, Xfinal, b, tol, N)
+        implicit none
+        integer, intent(in) :: n                     ! Dimensión del problema A(n,n) b(n) X(n)
+        ! Argumentos de la subrutina
+        real(8), intent(in) :: A(n,n)          !
+        real(8), intent(in) :: b(n)            ! DIMENSIONES ASUMIDAS
+        real(8), intent(out) :: Xfinal(:)    !
+        real(8), intent(in) :: tol             !
+            
+        ! Variables locales
+    
+        real(8), allocatable :: x0(:)
+        real(8), allocatable :: x(:)
+        real(8) :: sum1, sum2
+        integer :: iter, i, j, maxiter
+        
+        allocate(x0(n))
+        allocate(x(n))
+                    
+        !Primera semilla (primer valor establecido de x para iniciar la iteración)
+        x0 = 0.0d0
+        x  = 0.0d0
+        maxiter = 9999999 !Numero de iteraciones
+        do iter = 1, maxiter !Iteraciones
+            do i=1,n
+                sum1=0  
+                sum2=0            
+                do j=1,i-1
+                    sum1 = sum1 + A(i,j) * x(j)
+                enddo
+                do j=i+1,n
+                    sum2 = sum2 + A(i,j) * x0(j)
+                enddo                           
+                x(i) = (1/A(i,i)) * (B(i) - sum1 - sum2)           
+            enddo        
+            !if (norma2((x-x0), n)/norma2(x, n) <= tol) then
+                !Xfinal = x
+                !return
+            !else 
+                !x0 = x
+            !endif     
+        enddo      
+end subroutine resolver_gauss_seidel_tol
