ad1ba4c73779cf2dd18e586a5fe382d3d32ccf75
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-03-18 22:05:35 +0100

    alex

--- lib/alg_autovalores.f95
+++ lib/alg_autovalores.f95
@@ -7,7 +7,7 @@ subroutine auto_potencia (A, AUTOVALOR, TOL, Q0, N)
 
     ! Variables de entrada/salida
     integer, intent(in) :: N
-    real*8, intent(in)  :: A(N,N), TOL, Q0(N)
+    real*8, intent(in)  :: A(N,N), TOL, Q0(N)   !SELECCIONAR UN q0 (1.d0 por lo general)
     real*8, intent(out) :: AUTOVALOR
 
     ! Variables propias
@@ -15,9 +15,6 @@ subroutine auto_potencia (A, AUTOVALOR, TOL, Q0, N)
     integer :: i, maxiter
 
     maxiter = 10
-
-    !   SELECCIONAR UN q0
-    !Q=(1.d0)
 
     Q = Q0
     call norma2(norma,Q,N)
@@ -32,6 +29,7 @@ subroutine auto_potencia (A, AUTOVALOR, TOL, Q0, N)
         Q_ANTERIOR = Q
 
         Q = matmul(A,Q)
+
         call norma2(norma,Q,N)
         Q = Q/norma
         write(*,*) Q
@@ -55,11 +53,42 @@ subroutine auto_potencia_inversa (A, AUTOVALOR, TOL, Q0, N)
 
     ! Variables de entrada/salida
     integer, intent(in) :: N
-    real*8, intent(in)  :: A(N,N), TOL, Q0(N)
+    real*8, intent(in)  :: A(N,N), TOL, Q0(N)    !SELECCIONAR UN q0 (1.d0 por lo general)
     real*8, intent(out) :: AUTOVALOR
 
     ! Variables propias
     real*8 :: Q(N), Q_ANTERIOR(N), norma, resto(N), AUTOVECTOR(N)
     integer :: i, maxiter
+
+    maxiter = 10
+
+    Q = Q0
+    call norma2(norma,Q,N)
+    if (norma /= 1.d0) then
+        Q = Q/norma
+    endif
+    write(*,*) Q
+
+    !   CALCULO DEL AUTOVECTOR
+    do i = 1, maxiter
+
+        Q_ANTERIOR = Q
+
+        call resolver_gauss (A, Q_ANTERIOR, Q, N)
+
+        call norma2(norma,Q,N)
+        Q = Q/norma
+        write(*,*) Q
+        
+        resto = Q - Q_ANTERIOR
+
+        if( maxval(resto) <tol) then
+            AUTOVECTOR = Q
+            exit
+        endif
+    enddo
+    
+    !   CALCULO DEL AUTOVALOR ASOCIADO (coef. de Rayleigh)
+    AUTOVALOR = DOT_PRODUCT(AUTOVECTOR, matmul(A, AUTOVECTOR)) / DOT_PRODUCT(AUTOVECTOR, AUTOVECTOR)
 
 end subroutine auto_potencia_inversa
