181a3b1d7c0249b1a8d22f46cb3776d426e2eff3
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-03-19 22:25:59 +0100

    alex

--- Test Iter/main.f95
+++ Test Iter/main.f95
@@ -1,24 +1,27 @@
 program autovalores
     implicit none
-    real*8, allocatable :: A(:,:), B(:), tol, sol(:)
+    real*8, allocatable :: A(:,:), B(:), T(:,:)
     integer :: N
+    logical :: logic
 
     ! Se piden los datos de la matriz y q0 al usuario
     write(*,*) "¿Que N desea?"
     read(*,*) N
     allocate(A(N,N))
     allocate(B(N))
-    allocate(sol(N))
+    allocate(T(N,N))
     call pedir_matrix(A,N)
     call pedir_vector(B,N)
     call write_AB(A,B,N)
     ! -----------------------------------------
 
-    tol = 0.0005d0
-    call resolver_jacobi_tol(A, sol, B, tol, N)
-    write(*,*) "Jacobi:", sol
-    call resolver_gauss_seidel_tol(A, sol, B, tol, N)
-    write(*,*) "Gauss-Seidel:", sol
-    call resolver_gauss(A, B, sol, N)
-    write(*,*) "Gauss", sol
+    call matrizT_jacobi (A, T, N)
+    call convergencia (T, logic, N)
+    call write_A_int (T, N)
+    write(*,*) logic
+    call matrizT_gauss_seidel (A, T, N)
+    call convergencia (T, logic, N)
+    call write_A_int (T, N)
+    write(*,*) logic
+
 end program autovalores
