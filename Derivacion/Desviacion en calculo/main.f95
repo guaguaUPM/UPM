f92297e0526947aebeeb9b30166276a9289a08d7
Fernando Ayats <ayatsfer@gmail.com>
2018-03-20 09:24:45 +0100

    calclulo de error en derivada

--- Derivacion/Desviacion en calculo/main.f95
+++ Derivacion/Desviacion en calculo/main.f95
@@ -2,16 +2,20 @@ program main
 use funciones
 implicit none
 
-real*8 :: m, x0, h, epsi
-integer :: i
+real*8 :: m, x0, h
+integer :: i, expINICIO, expFINAL, particiones
 
 write(*,*) "En que punto desea la derivada?"
 read(*,*) x0
-epsi = EPSILON(x0)
+
+expINICIO = -14
+expFINAL = -17
+particiones = 100
+
+
 open(unit=10,file='error.dat',status='unknown',action='write')
-h = 1.d-5
-do i = -5, -16, -1
-    h=10.d0**(i)
+do i = 1, particiones
+    h=10.d0**( (expFINAL-expINICIO)/(particiones*1.d0) * (i-1) + expINICIO )
     write(*,*) h
     call derivada1_cent2(seno,x0,m,h)
     write(*,*) cos(x0), m, cos(x0)-m
@@ -20,8 +24,8 @@ end do
 close(10)
 
 open(unit=11,file='ref.dat',status='unknown',action='write')
-write(11,*) 1.d-5, 0.d0
-write(11,*) 1.d-16, 0.d0
+write(11,*) 10.d0**(expINICIO), 0.d0
+write(11,*) 10.d0**(expFINAL), 0.d0
 close(11)
 
 call SYSTEM("python plot11_derivada.py")
