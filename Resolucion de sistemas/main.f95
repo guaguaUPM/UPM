093dd1dae165ee391dda0a54674283b3568e7326
Fernando Ayats <ayatsfer@gmail.com>
2018-03-21 19:00:38 +0100

    syntax

--- Resolucion de sistemas/main.f95
+++ Resolucion de sistemas/main.f95
@@ -17,23 +17,23 @@ call write_AB(A,B, N)
 write(*,*)
 ! ------------------------------
 
-call resolver_gauss(A,B,X,N)
+call resolver_gauss             (A,B,X,N)
 write(*,*) "Resolucion por Gauss:"
 write(*,*) X
 
-call resolver_LU(A,B,X,N)
+call resolver_LU                (A,B,X,N)
 write(*,*) "Resolucion por LU:"
 write(*,*) X
 
-call resolver_jacobi_iter (A,B,X,N,10000)
+call resolver_jacobi_iter        (A,B,X,N,10000)
 write(*,*) "Resolución por Jacobi, 10000 iteraciones"
 write(*,*) X
 
-call resolver_gauss_seidel_iter (A,B,X,N,10000)
+call resolver_gauss_seidel_iter  (A,B,X,N,10000)
 write(*,*) "Resolución por Gauss-Seidel, 10000 iteraciones"
 write(*,*) X
 
-call resolver_LAPACK(A,B,X,N)
+call resolver_LAPACK             (A,B,X,N) ! Cuidado que A sale modificada
 write(*,*) "Resolucion por LAPACK:"
 write(*,*) X
 
