c64e63f5399af5e5ed657d7644c82c6d07098d39
guaguaUPM <guagua.contact@gmail.com>
2018-03-22 13:35:16 +0100

    lu arreglao

--- lib/alg_LU.f95
+++ lib/alg_LU.f95
@@ -16,22 +16,25 @@ subroutine LU_factorizar (A,L,U,N)
     U(1,1) = 1
     do k=1,n-1
         U(1,k+1) = A(1,k+1)/L(1,1)   
-        do i=2,k      
+        do i=2,k     
+            sum=0
             do j=1,i-1
-                sum = L(i,j)*U(j,k+1)
+                sum = L(i,j)*U(j,k+1)+sum
             end do 
             U(i,k+1) = (A(i,k+1)-sum)/L(i,i)
         end do  
         L(k+1,1) = A(k+1,1)
         do i=2,k      
+            sum=0
             do j=1,i-1
-                sum = U(j,i)*L(k+1,j)
+                sum = U(j,i)*L(k+1,j)+sum
             end do 
             L(k+1,i) = (A(k+1,i)-sum)
         end do
-        U(k+1,k+1) = 1                              
+        U(k+1,k+1) = 1     
+        sum=0                         
         do i=1,k
-            sum = L(k+1,i)*U(i,k+1)     
+            sum = L(k+1,i)*U(i,k+1)+sum
         end do
         L(k+1,k+1) = A(k+1,k+1)-sum   
     end do
