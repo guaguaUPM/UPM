ce9b1281f33df7472f7b7ce14a919907ffedfc28
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-03-23 16:08:59 +0100

    alx

--- Problema de calor/main.f95
+++ Problema de calor/main.f95
@@ -6,11 +6,11 @@ implicit none
 
 real*8, allocatable :: matriz(:,:)
 real*8 :: x1, x2
-integer, parameter :: n=100
+integer, parameter :: n=10
 
 !DEBUG
-x1=100.d0
-x2=200.d0
+x1=10.d0
+x2=20.d0
 allocate(matriz(n,n))
 call matrizcontorno(matriz, n, x1, x2)
 call write_A(matriz, n)
--- lib/calc_diferenciales.f95
+++ lib/calc_diferenciales.f95
@@ -1,27 +1,27 @@
-subroutine matrizcontorno (MATRIZ, NINTERVALOS, X1, X2)
+subroutine matrizcontorno (MATRIZ, PARTICIONES, X1, X2)
     ! SUBRUTINA QUE CALCULA UNA MATRIZ DE CONTORNO GENERICA DADO EL
-    ! NUMERO DE INTERVALOS EN LOS QUE SE QUIERA DISCRETIZAR NUESTRA VARIABLE
+    ! NUMERO DE PARTICIONES EN LOS QUE SE QUIERA DISCRETIZAR NUESTRA VARIABLE
     ! Y EL TAMAÑO DEL INCREMENTO (DEDUCIDO A TRAVES DEL PUNTO INICIAL Y FINAL)
     implicit none
 
     ! ARGUMENTOS
     real*8, intent(in)  :: X1, X2
-    integer, intent(in) :: NINTERVALOS
-    real*8, intent(out) :: MATRIZ(NINTERVALOS,NINTERVALOS)
+    integer, intent(in) :: PARTICIONES
+    real*8, intent(out) :: MATRIZ(PARTICIONES,PARTICIONES)
 
     ! VARIABLES LOCALES
     real*8              :: incremento
     integer             :: i, j
 
-    incremento = (X2-X1)/(NINTERVALOS*1.d0)
+    incremento = (X2-X1)/(PARTICIONES*1.d0)
 
     MATRIZ = 0.d0
     MATRIZ(1,2) = 1/(incremento*1.d0)
     MATRIZ(1,1) = -1/(incremento*1.d0)
-    MATRIZ(NINTERVALOS,NINTERVALOS) = 1/(incremento*1.d0)
-    MATRIZ(NINTERVALOS,NINTERVALOS-1) = -1/(incremento*1.d0)
-    do i=2, NINTERVALOS-1
-        do j=2, NINTERVALOS-1
+    MATRIZ(PARTICIONES,PARTICIONES) = 1/(incremento*1.d0)
+    MATRIZ(PARTICIONES,PARTICIONES-1) = -1/(incremento*1.d0)
+    do i=2, PARTICIONES-1
+        do j=2, PARTICIONES-1
             MATRIZ(i,i+1) = 1/(2.d0*incremento)
             MATRIZ(i,i-1) = -1/(2.d0*incremento)
         enddo
