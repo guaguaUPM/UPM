95b325f59604d87a6298133943fb904de308091f
Fernando Ayats <ayatsfer@gmail.com>
2018-04-03 11:53:37 +0100

    no funcionaba xD

--- Problema de calor/puntos.f95
+++ Problema de calor/puntos.f95
@@ -4,10 +4,11 @@ subroutine datos_a_puntos(VECTOR,INCREMENTO,X0,N)
     real*8, intent(in) :: VECTOR(N), INCREMENTO, X0
 
     real*8 :: x
+    integer :: i
 
     x = X0
 
-    open(unit=10,file='puntos.dat',status='unknown',action='write',iostat=info)
+    open(unit=10,file='puntos.dat',status='unknown',action='write')
     do i = 1, N
         write(10,*) x, VECTOR(N)
         x = x * INCREMENTO*i
