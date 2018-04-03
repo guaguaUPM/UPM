79d4faf5d95f23392b3f11f33a68d93be07004dc
Fernando Ayats <ayatsfer@gmail.com>
2018-04-03 11:49:09 +0100

    representacion del problema de contorno

---
+++ Problema de calor/puntos.f95
@@ -0,0 +1,16 @@
+subroutine datos_a_puntos(VECTOR,INCREMENTO,X0,N)
+    implicit none
+    integer, intent(in) :: N
+    real*8, intent(in) :: VECTOR(N), INCREMENTO, X0
+
+    real*8 :: x
+
+    x = X0
+
+    open(unit=10,file='puntos.dat',status='unknown',action='write',iostat=info)
+    do i = 1, N
+        write(10,*) x, VECTOR(N)
+        x = x * INCREMENTO*i
+    end do
+    close(10)
+end subroutine datos_a_puntos
