c5c9d92208ca6e0e4e4b8eed29bc200f4d3fe566
Fernando Ayats <ayatsfer@gmail.com>
2018-04-03 17:27:26 +0100

    framework lucas

--- Test corte/funciones.f95
+++ Test corte/funciones.f95
@@ -1,44 +1,15 @@
 module funciones
 contains
-    function parabola(X)
+    function principal(X)
         real*8 :: X
-        real*8 :: parabola
+        real*8 :: principal
 
-        parabola = X**2
-    end function parabola
-    function arcotangente(X)
+        principal = X**3 - exp(1.d0)
+    end function principal
+    function secundaria(X)
         real*8 :: X
-        real*8 :: arcotangente
-    
-        arcotangente = ATAN(X)
-    end function arcotangente
-    function paraboloide(X,Y)
-        real*8 :: X,Y
-        real*8 :: paraboloide
+        real*8 :: secundaria
 
-        paraboloide = X**2 + Y**2 + 2
-    end function paraboloide
-    function f001(X)
-        real*8 :: X
-        real*8 :: f001
-      
-        f001 = X*sin(X)
-    end function f001
-    function seno(X)
-        real*8 :: X
-        real*8 :: seno
-      
-        seno = sin(X)
-    end function seno
-    function logaritmo(X)
-        real*8 :: x
-        real*8 :: logaritmo
-
-        logaritmo = log(x)
-    end function logaritmo
-    function xcubo(x)
-        real*8 :: x
-        real*8 :: xcubo
-        xcubo = x**3
-    end function xcubo
+        secundaria = 3.d0 * x**2
+    end function secundaria
 end module funciones
--- Test corte/main.f95
+++ Test corte/main.f95
@@ -4,15 +4,25 @@ implicit none
 
 real*8 :: x(2), tol, corte
 
+write(*,*) "FUNCION:  x^3 - e"
+! -------------
 write(*,*) "Usando el modo bissecion, introduzca los dos puntos de contorno:"
 read(*,*) x(1)
 read(*,*) x(2)
-
 write(*,*) "¿Que toleracia desea?"
 read(*,*) tol
 
-call corte_biseccion(xcubo,x(1),x(2),tol,corte)
+call corte_biseccion(principal,x(1),x(2),tol,corte)
+write(*,*) "RESULTADO:", corte
 
-write(*,*) corte
+write(*,*)
+! --------------
+write(*,*) "Usando el metodo de newton con derivada analitica, introduzca el punto de partida:"
+read(*,*) x(1)
+write(*,*) "Usando la misma tolerancia que antes..."
+! read(*,*) tol
+
+call corte_newton_anal(principal, secundaria, x(1), tol, corte)
+write(*,*) "RESULTADO:", corte
 
 end program main
