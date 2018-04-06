50dd79f86927db4fff1441e657b93b345d4278a4
Fernando Ayats <ayatsfer@gmail.com>
2018-04-06 15:30:51 +0200

    particiones arreglada

--- Problema de calor/puntos.f95
+++ Problema de calor/puntos.f95
@@ -1,17 +1,14 @@
-subroutine datos_a_puntos(VECTOR,INCREMENTO,X0,N)
+subroutine datos_a_puntos(VECTOR,N)
     implicit none
     integer, intent(in) :: N
     real*8, intent(in) :: VECTOR(N), INCREMENTO, X0
 
-    real*8 :: x
     integer :: i
 
-    x = X0
 
     open(unit=10,file='puntos.dat',status='unknown',action='write')
     do i = 1, N
-        write(10,*) x, VECTOR(N)
-        x = x + INCREMENTO
+        write(10,*) i, VECTOR(N)
     end do
     close(10)
 end subroutine datos_a_puntos
