77c8e07fe4cd2c77c339afe33219f9df45492cac
alvaro234 <alvaroglezvilla@gmail.com>
2018-04-06 15:41:40 +0200

    a

--- Problema de calor/puntos.f95
+++ Problema de calor/puntos.f95
@@ -8,7 +8,7 @@ subroutine datos_a_puntos(VECTOR,N)
 
     open(unit=10,file='puntos.dat',status='unknown',action='write')
     do i = 1, N
-        write(10,*) Vector(N), i
+        write(10,*) i, VECTOR(i)
     end do
     close(10)
 end subroutine datos_a_puntos
