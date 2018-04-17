0e423eecb64b237b8772901e1650344fd0ed921b
Fernando Ayats <ayatsfer@gmail.com>
2018-04-17 11:54:50 +0200

    funcion

--- JULIA SET/funciones.f95
+++ JULIA SET/funciones.f95
@@ -12,4 +12,12 @@ function jacobiano(X,Y)
     jacobiano(2,2) = 3*(X**2) - 3*(Y**2)
 end function jacobiano
 
+function F(X,Y)
+    real*8 :: F(2)
+    real*8,intent(in) :: X, Y
+
+    F(1) = X**3 - 3*X*(Y**2) - 1.d0/3.d0
+    F(2) = 3*(X**2)*Y - Y**3 
+end function F
+
 end module funciones
