2b576764714a3ad1662306af162f8af55bd10ea7
guaguaUPM <36190441+guaguaUPM@users.noreply.github.com>
2018-05-14 14:08:38 +0200

    alex

---
+++ ZOMBIE PROJECT/plot_R.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+T, S, R, Z = [], [], [], []
+for line in open('T_SRZ.dat', 'r'):
+  values = [float(s) for s in line.split()]
+  T.append(values[0])
+  S.append(values[1])
+  R.append(values[2])
+  Z.append(values[3])
+
+plt.plot(T,R,'b-')
+plt.show()
